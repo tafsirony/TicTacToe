55323ea305292ed77f223c19b0b3bd6a41efd7a4
Tafsir Ony <onykhan95@gmail.com>
2019-10-20 23:18:02 +0600

    Activity Game Done

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,12 +3,147 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button[][] btn = new Button[3][3];
+
+    private boolean player1Turn = true;
+    private int roundCount;
+    private int player1Points;
+    private int player2Points;
+
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewPlayer1 = findViewById(R.id.text_view_1);
+        textViewPlayer2 = findViewById(R.id.text_view_2);
+
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                String btnID = "btn_"+ i +j;
+                int resID = getResources().getIdentifier(btnID,"id",getPackageName());
+                btn[i][j] = findViewById(resID);
+                btn[i][j].setOnClickListener(this);
+
+            }
+        }
+        Button btnReset = findViewById(R.id.btn_reset);
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetGame();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(!((Button)v).getText().toString().equals("")) {
+            return;
+        }
+        if(player1Turn) {
+            ((Button) v).setText("");
+        }else {
+            ((Button) v).setText("O");
+        }
+        roundCount++;
+        if(checkForWin()) {
+            if(player1Turn) {
+                player1Wins();
+            }else {
+                player2Wins();
+            }
+        }else if(roundCount==9) {
+            draw();
+        }else {
+            player1Turn =!player1Turn;
+        }
+
+    }
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = btn[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+    private void player1Wins(){
+        player1Points++;
+        Toast.makeText(this,"player 1 wins",Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+    private void player2Wins(){
+        player2Points++;
+        Toast.makeText(this,"player 2 wins",Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+    private void draw(){
+        Toast.makeText(this,"Draw!",Toast.LENGTH_SHORT);
+        resetBoard();
+    }
+    private void updatePointsText(){
+        textViewPlayer1.setText("player 1: "+player1Points);
+        textViewPlayer2.setText("player 2: "+player2Points);
+    }
+    private void resetBoard(){
+        for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                btn[i][j].setText("");
+            }
+        }
+        roundCount = 0;
+        player1Turn = true;
+    }
+    private void resetGame() {
+        player1Points= 0;
+        player2Points= 0;
+        updatePointsText();
+        resetBoard();
     }
 }
+
